a2f67ebf2f0c7ed5e08b4fca0c427459b77f0a1e
이재원 <jenyglee_30@naver.com>
2022-11-23 17:05:29 +0900

    feat : MemoList > delete 삭제 메서드 print 인터페이스 정리 fix : Main 실행문 while에서 'break' 코드 삭제

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,7 @@ public class Main {
         MemoList memoList = new MemoList();
 
         while (true) {
-            System.out.println("숫자를 입력해주세요.");
+            System.out.println("--------------숫자를 입력해주세요.--------------");
             System.out.println("1. 입력");
             System.out.println("2. 목록 보기");
             System.out.println("3. 수정");
@@ -19,17 +19,12 @@ public class Main {
 
             if (select_Num.equals("1")) {
                 System.out.println("메모를 입력합니다.");
-                break;
             } else if (select_Num.equals("2")) {
                 System.out.println("메모 목록을 확인합니다.");
-                break;
             } else if (select_Num.equals("3")) {
                 System.out.println("메모를 수정합니다.");
-                break;
             } else if (select_Num.equals("4")) {
-                System.out.println("메모를 삭제합니다.");
                 memoList.delete();
-                break;
             } else if (select_Num.equals("5")) {
                 System.exit(0);
             } else {
--- src/main/java/MemoList.java
+++ src/main/java/MemoList.java
@@ -15,7 +15,6 @@ public class MemoList implements Memo_input, Memo_update, Memo_delete {
 
     @Override
     public void delete() {
-        System.out.println("재원 작업공간");
         int searchId;
         String password;
 
@@ -33,20 +32,32 @@ public class MemoList implements Memo_input, Memo_update, Memo_delete {
         for(int i=0; i<memoData.size(); i++){
             if(searchId == memoData.get(i).num){
                 int itemNum = memoData.get(i).num;
-                System.out.println(itemNum + "번 글을 찾았습니다.");
-                System.out.print("비밀번호 입력 : ");
+                System.out.println("✨" + itemNum + "번 글을 찾았습니다.");
+                System.out.println("내용 : "  + memoData.get(i).content);
+                System.out.print("삭제하려면 비밀번호 입력 : ");
                 password = sc.nextLine();
 
                 if(memoData.get(i).password.equals(password)){
                     memoData.remove(i);
-                    System.out.println(itemNum + "번 글을 삭제했습니다");
+                    System.out.println("✨" + itemNum + "번 글을 삭제했습니다");
+                    System.out.println("✨남은 메모리스트 ======================");
+                    // 메모 삭제 후 num을 앞당겨주는 기능
                     for (int j=0; j<memoData.size(); j++){
-                        System.out.println("num : " + memoData.get(j).num + ", content : " + memoData.get(j).content);
+                        if(j < i){
+                            memoData.set(j, new Memo(memoData.get(j).num, memoData.get(j).name, memoData.get(j).password, memoData.get(j).content));
+                        }else{
+                            memoData.set(j, new Memo(memoData.get(j).num-1, memoData.get(j).name, memoData.get(j).password, memoData.get(j).content));
+                        }
+
+                        System.out.println("num : " + memoData.get(j).num + ", name : " + memoData.get(j).name + ", content : " + memoData.get(j).content);
                     }
+                    System.out.println("====================================");
+                    System.out.println("입력 끝");
                 }else{
                     System.out.println("비밀번호가 틀렸습니다.");
                 }
             }
         }
+
     }
 }
