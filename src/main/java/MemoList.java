2cf5495c3d693b7f06217b85fa9e46b8c5f27e14
이재원 <jenyglee_30@naver.com>
2022-11-23 14:01:26 +0900

    Feat : MemoList > delete 메소드 구현 진행중

--- src/main/java/MemoList.java
+++ src/main/java/MemoList.java
@@ -1,5 +1,9 @@
-public class MemoList implements Memo_input, Memo_update, Memo_delete {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class MemoList implements Memo_input, Memo_update, Memo_delete {
+    int searchId;
+    String password;
 //    여러 건의 메모를 저장하는 클래스 이다.
 //    메모 전체를 조회할 수 있다. (Getter/Setter 존재)
 //    작성 최신 순으로 메모를 출력한다.
@@ -13,5 +17,32 @@ public class MemoList implements Memo_input, Memo_update, Memo_delete {
     @Override
     public void delete() {
         System.out.println("재원 작업공간");
+        ArrayList<Memo> memoData = new ArrayList<Memo>();
+        Memo memo1 = new Memo(1, "이재원", "1234", "내용");
+        Memo memo2 = new Memo(2, "이재원2", "1234", "내용2");
+        Memo memo3 = new Memo(3, "이재원3", "1234", "내용3");
+        memoData.add(memo1);
+        memoData.add(memo2);
+        memoData.add(memo3);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("글 번호 입력 : ");
+        searchId = sc.nextInt();
+        for(int i=0; i<memoData.size(); i++){
+            if(searchId == memoData.get(i).num){
+                System.out.println("내가 찾은 글은");
+                System.out.println(memoData.get(i).num);
+                System.out.println(memoData.get(i).name);
+                System.out.println(memoData.get(i).content);
+                System.out.println(memoData.get(i).password);
+                System.out.print("비밀번호 입력 : ");
+                password = sc.nextLine();
+                if(password == memoData.get(i).password){
+                    System.out.println("삭제시작");
+                }else System.out.println("비밀번호가 틀렸습니다.");
+            }
+        }
+
+
     }
 }
