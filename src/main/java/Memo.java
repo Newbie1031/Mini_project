f8c21698c95bbb997c3ff629eade8f46a8aaa651
Park Kyungwon <107618489+ruddnjs5816@users.noreply.github.com>
2022-11-23 16:51:49 +0900

    업데이트 추가

--- src/main/java/Memo.java
+++ src/main/java/Memo.java
@@ -1,3 +1,5 @@
+import java.time.LocalDateTime;
+
 public class Memo {
     //    메모 한 건에 대한 정보를 가진 클래스
     //    메모는 글 번호, 작성자 이름, 비밀번호, 게시글, 작성일(컴퓨터 시스템의 날짜와 시간을 자동으로 저장) 가지고 있다.
@@ -7,12 +9,21 @@ public class Memo {
     String password;
     String content;
 
+    LocalDateTime creteAt;
     public Memo(int num, String name, String password, String content) {
         this.num = num;
         this.name = name;
         this.password = password;
         this.content = content;
+//        this.creteAt = LocalDateTime.now();
     }
 
+    void update(String name) {
+        this.num = num;
+        this.name = name;
+        this.password = password;
+        this.content = content;
+        this.creteAt= LocalDateTime.now();
+    }
 
 }
--- src/main/java/Memo_update.java
+++ src/main/java/Memo_update.java
@@ -1,9 +1,41 @@
-public interface Memo_update {
+import java.util.Scanner;
 
+public interface Memo_update {
 //    수정할 글 번호를 입력 받는다.
 //    수정할 글이 존재하면 글을 수정하고 존재하지 않으면 존재하지 않는다고 메시지를 출력한다.
 //    수정할 글의 비밀번호를 입력 받는다.
 //    글을 수정하기 위해 입력한 비밀번호와 수정할 글의 비밀번호가 일치하면 글을 수정하고 일치하지 않으면 비밀번호가 일치하지 않는다는 메시지를 출력한다.
-//            비밀번호가 일치할 시 : 수정 내용 업데이트
-//            비밀번호 불일치 시 : 비밀번호가 일치하지 않는다는 메시지 출력
+//    비밀번호가 일치할 시 : 수정 내용 업데이트
+//    비밀번호 불일치 시 : 비밀번호가 일치하지 않는다는 메시지 출력
+
+    public default void updateNote() {
+        System.out.println("");
+        Scanner scanner = new Scanner(System.in);
+        Memo[] memoData = new Memo[30];
+
+        System.out.println("비밀번호를 입력해 주세요");
+        int selectPassword = Integer.parseInt(scanner.nextLine());
+
+        if (selectPassword!=memoData[]){
+            System.out.println("비밀번호가 일치하지 않습니다.");
+        }else{
+            System.out.println("수정하실 메모의 번호를 입력해주세요");
+            int selectedNumber = Integer.parseInt(scanner.nextLine());
+
+            System.out.println("본문을 작성해주세요");
+            String content = scanner.nextLine();
+
+            Memo noteEntity = memoData[selectedNumber];
+
+            if (null == noteEntity) {
+                System.out.println("존재하지 않는 메모입니다");
+                return;
+            }
+            noteEntity.update(content);
+
+            System.out.println("메모가 수정되었습니다");
+            System.out.println("");
+        }
+    }
 }
+
