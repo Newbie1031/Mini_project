8ada8e3fa77148c88c78771acda46d0ce0d151dd
Park SangUk <psw1375@gmail.com>
2022-11-23 20:20:04 +0900

    콘솔창 표시 통합

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,12 +8,12 @@ public class Main {
         MemoList memoList = new MemoList();
 
         while (true) {
-            System.out.println("--------------숫자를 입력해주세요.--------------");
-            System.out.println("1. 입력");
-            System.out.println("2. 목록 보기");
-            System.out.println("3. 수정");
-            System.out.println("4. 삭제");
-            System.out.println("5. 종료");
+            System.out.println("--- 해당하는 메뉴의 숫자를 입력하세요. ---");
+            System.out.println("1 : 메모 입력");
+            System.out.println("2 : 메모 목록");
+            System.out.println("3 : 메모 수정");
+            System.out.println("4 : 메모 삭제");
+            System.out.print("5 : 메모장 종료\n→ ");
 
             select_Num = sc.nextLine();
 
@@ -26,9 +26,10 @@ public class Main {
             } else if (select_Num.equals("4")) {
                 memoList.delete();
             } else if (select_Num.equals("5")) {
+                System.out.println("\n✨ 메모장을 종료합니다.");
                 System.exit(0);
             } else {
-                System.out.println("숫자를 다시 확인해주세요.");
+                System.out.println("✨ 해당 숫자의 메뉴가 존재하지 않습니다.");
             }
         }
     }
--- src/main/java/MemoList.java
+++ src/main/java/MemoList.java
@@ -27,28 +27,28 @@ public class MemoList implements Memo_input, Memo_read,  Memo_delete {
         int num = 0;
 
 
-        System.out.print("이름을 입력하세요.\n→");
+        System.out.print("\n✨ 이름을 입력하세요.\n→ ");
         name_input = sc.nextLine();
-        System.out.print("비밀번호를 입력하세요.\n→");
+        System.out.print("\n✨ 비밀번호를 입력하세요.\n→ ");
         password_input = sc.nextLine();
-        System.out.print("메모를 입력하세요.\n→");
+        System.out.print("\n✨ 메모 내용을 입력하세요.\n→ ");
         content_input = sc.nextLine();
         memoDate.add(new Memo(memoDate.size() + 1, name_input, password_input, content_input));
+        System.out.println("\n✨ 메모가 저장되었습니다.\n");
     }
 
 
         @Override
         public void read() {
 
-            System.out.println("번호와 내용이 출력됩니다.");
+            System.out.println("\n✨ 메모 번호와 내용을 출력합니다.\n");
             // 모두 출력
             for (int i = 0; i < memoDate.size(); i++) {
-                System.out.println("-----" + (i + 1) + "번 메모" + "-----");
-                System.out.println("번호: " + memoDate.get(i).getNum());
-                System.out.println("내용: " + memoDate.get(i).getContent());
-                System.out.println();
+                System.out.println("---------- " + (i + 1) + "번 메모" + " ----------");
+                System.out.println("✨ 번호 : " + memoDate.get(i).getNum());
+                System.out.println("✨ 내용 : " + memoDate.get(i).getContent());
             }
-
+            System.out.println("---------- 출력 완료 ----------\n");
         }
 
         @Override
@@ -57,20 +57,20 @@ public class MemoList implements Memo_input, Memo_read,  Memo_delete {
             String password;
 
             Scanner sc = new Scanner(System.in);
-            System.out.print("글 번호 입력 : ");
+            System.out.print("\n✨ 메모 번호를 입력하세요.\n→ ");
             searchId = Integer.parseInt(sc.nextLine());
             for (int i = 0; i < memoDate.size(); i++) {
                 if (searchId == memoDate.get(i).num) {
                     int itemNum = memoDate.get(i).num;
-                    System.out.println("✨" + itemNum + "번 글을 찾았습니다.");
-                    System.out.println("내용 : " + memoDate.get(i).content);
-                    System.out.print("삭제하려면 비밀번호 입력 : ");
+                    System.out.println("\n✨ " + itemNum + "번 메모를 찾았습니다.\n");
+                    System.out.println("✨ 내용 : " + memoDate.get(i).content);
+                    System.out.print("\n✨ 삭제하려면 비밀번호를 입력하세요.\n→ ");
                     password = sc.nextLine();
 
                     if (memoDate.get(i).password.equals(password)) {
                         memoDate.remove(i);
-                        System.out.println("✨" + itemNum + "번 글을 삭제했습니다");
-                        System.out.println("✨남은 메모리스트 ======================");
+                        System.out.println("\n✨ " + itemNum + "번 메모를 삭제했습니다.\n");
+                        System.out.println("========== 현재 저장된 메모 리스트 ==========");
                         // 메모 삭제 후 num을 앞당겨주는 기능
                         for (int j = 0; j < memoDate.size(); j++) {
                             if (j < i) {
@@ -79,10 +79,10 @@ public class MemoList implements Memo_input, Memo_read,  Memo_delete {
                                 memoDate.set(j, new Memo(memoDate.get(j).num - 1, memoDate.get(j).name, memoDate.get(j).password, memoDate.get(j).content));
                             }
 
-                            System.out.println("num : " + memoDate.get(j).num + ", name : " + memoDate.get(j).name + ", content : " + memoDate.get(j).content);
+                            System.out.println("✨ 번호 : " + memoDate.get(j).num + ", 이름 : " + memoDate.get(j).name + ", 내용 : " + memoDate.get(j).content + "\n");
                         }
                     } else {
-                        System.out.println("비밀번호가 틀렸습니다.");
+                        System.out.println("\n✨ 비밀번호를 잘못 입력하셨습니다.\n");
                     }
                 }
             }
@@ -98,24 +98,24 @@ public class MemoList implements Memo_input, Memo_read,  Memo_delete {
 //        memoData.add(memo2);
 //        memoData.add(memo3);
 
-        System.out.println("");
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("수정하실 글 번호를 입력해주세요");
+        System.out.print("\n✨ 수정하실 메모 번호를 입력하세요.\n→ ");
         int selectNumber = Integer.parseInt(scanner.nextLine());
 
         for (int i = 0; i < memoDate.size(); i++) {
             if (selectNumber == memoDate.get(i).num) {
-                System.out.println("비밀번호를 입력해 주세요");
+                System.out.print("\n✨ 비밀번호를 입력하세요.\n→ ");
                 String selectPassword = scanner.nextLine();
 
                 for (int j = 0; j < memoDate.size(); j++) {
                     if (memoDate.get(j).password.equals(selectPassword)) { // String 한정
-                        System.out.println("본문을 작성해주세요");
+                        System.out.print("\n✨ 내용을 작성하세요.\n→ ");
                         String content = scanner.nextLine();
 
                         memoDate.set(j, new Memo(memoDate.get(j).num, memoDate.get(j).name, memoDate.get(j).password, content));
-                        System.out.println("메모가 수정되었습니다");
+                        System.out.println("\n✨ 메모가 수정되었습니다.");
                         return;
 //                        System.out.println("");
                     }
